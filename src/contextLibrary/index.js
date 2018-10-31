f51c68978c8d1b0a90c25cd069aef72fe3e12ba1
antoniofpereir <antoniofpereir@gmail.com>
2018-10-31 23:33:42 +0000

    Update index.js

--- src/contextLibrary/index.js
+++ src/contextLibrary/index.js
@@ -13,18 +13,16 @@ export const AppContext = React.createContext();
  * Returns a wrapper component that will provide the context to all the children.
  */
 export function initContext(localStorageName, actions, ...data) {
-  return class GenericContext extends React.PureComponent {
+  return class GenericContext extends React.Component {
     constructor() {
       super();
       const newContext = Object.assign(...data);
-      const contextFromLocalStorage = JSON.parse(
-        localStorage.getItem(localStorageName)
-      );
+      this.contextFromLocalStorage = this.getContextFromLocalStorage();
 
       /**
        * Init context with newContext if contextFromLocalStorage is empty.
        */
-      this.state = contextFromLocalStorage || newContext;
+      this.state = this.contextFromLocalStorage || newContext;
 
       /**
        * Execute function decides functionalSetState based on actionType.
@@ -36,19 +34,47 @@ export function initContext(localStorageName, actions, ...data) {
     }
 
     /**
+     * Saves the state to localStorage after initializing for the first time.
+     */
+    componentDidMount() {
+      if (!this.contextFromLocalStorage) {
+        localStorage.setItem(localStorageName, JSON.stringify(this.state));
+      }
+    }
+
+    /**
      * Uses setState to update context component state and stores the new state in local storage.
      * @param {*} functionalSetState function that returns a state change.
      * @param {*} params used in functionalSetState (optional).
      */
-    setStateAndUpdateLocalStorage(functionalSetState, params) {
+    setStateAndUpdateLocalStorage = (functionalSetState, params) => {
       this.setState(
         prevState => functionalSetState(prevState, ...params),
-        () => {
-          localStorage.setItem(localStorageName, JSON.stringify(this.state));
-          console.log('Context in local storage updated to: ', this.state);
-        }
+        this.saveContextToLocalStorage()
       );
-    }
+    };
+
+    /**
+     * Retrieves context from localStorage.
+     * Surround with try-catch because user privacy settings might not allow local storage.
+     */
+    getContextFromLocalStorage = () => {
+      try {
+        return JSON.parse(localStorage.getItem(localStorageName));
+      } catch (err) {
+        console.log('Error reading from local storage');
+        return null;
+      }
+    };
+
+    saveContextToLocalStorage = () => {
+      try {
+        localStorage.setItem(localStorageName, JSON.stringify(this.state));
+        this.contextFromLocalStorage = this.getContextFromLocalStorage();
+      } catch (err) {
+        console.log('Error saving to local storage');
+      }
+    };
 
     /**
      * Wraps child components with context provider.
